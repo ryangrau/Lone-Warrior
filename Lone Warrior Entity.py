9bbf794fa08969f6220a5cdf3122b3fe1747e3c9
Ryan Grau <ryan.grau@gmail.com>
2016-04-26 16:49:08 -0700

    added 2 files
    Added class entity
    Added openNarr function

---
+++ Lone Warrior Entity.py
@@ -0,0 +1,24 @@
+
+        
+###
+
+class entity:
+        def _init_(self, name, currentHP, maxHP, attack, defence, equipment):
+                self.name = name
+                self.currentHP = int
+                self.maxHP = int
+                self.attack = int
+                self.defence = int
+                self.equipment = equipment
+
+hero = entity("name",100,100,18,10,"")
+wolf1 = entity("Kujo",100,100,20,10,"")
+wolf2 = entity("Fluffy",100,100,26,8,"")
+bossMagic = entity("Yomahmah",100,100,30,12,"")
+bossAxe = entity("Axesaw Duggin",100,100,38,6,"")
+bossMagicAxe = entity("Mahess",100,100,100,100,"")
+
+###
+                
+                
+
